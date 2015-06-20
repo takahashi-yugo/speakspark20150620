d27ddc9f35716a61349af0f5f5eea41225237aca
Yuko Katsuda <katsuda@mefilas.com>
2015-06-20 15:38:24 +0900

    jqueryを変更

--- js/index.js
+++ js/index.js
@@ -1,3 +1,51 @@
-(function(window, $) {
 
-})(this, jQuery);
+;(function(window, document){
+	var MAIN = function(){
+		var _self   = this;
+		_self.PARAM = {
+			$ELE : new Object(),
+			INT  : new Object()
+		};
+	};
+	
+	/* ----------------------------------------------------
+	 * Init
+	 * ------------------------------------------------- */
+	MAIN.prototype.Init = function Init(){
+		var _self = this;
+		
+		_self.PARAM.$ELE.scene1    = $('.section.scene1');
+		_self.PARAM.$ELE.Main      = $('#Main');
+
+		_self.PARAM.INT.window     = new Object();
+		_self.PARAM.INT.document   = new Object();
+
+		Main.Resize();
+		$(window).resize(function(){Main.Resize();});
+		
+	};
+	/* ----------------------------------------------------
+	 * Resize
+	 * ------------------------------------------------- */
+	MAIN.prototype.Resize = function Resize(){
+		var _self = this;
+
+		_self.PARAM.INT.window.width  = $(window).width();
+		_self.PARAM.INT.window.height = $(window).height();
+		_self.PARAM.INT.document.width  = $(document).width();
+		_self.PARAM.INT.document.height = $(document).height();
+	};
+	/* ----------------------------------------------------
+	 * MV
+	 * ------------------------------------------------- */
+	MAIN.prototype.MV = function MV(){
+		var _self = this;
+		
+	};
+	window.MAIN = MAIN;
+})(window, document);
+
+var Main = new MAIN();
+$(function(){
+	Main.Init();
+});
